d01457f131faaa179d1191ec86a3e0228efd02e6
Murray Colpman <muzer@tim32.org>
2014-11-07 19:53:44 +0000

    Add updater script

---
+++ updater.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+output="$(git pull --ff-only)"
+
+if [ "$?" != "0" ]; then
+  echo "Return value was evil" 2>> updater.log
+  exit 1
+fi
+
+if [ "$output" == "Already up-to-date." ]; then
+  echo "Already up-to-date." 2>&1 >> updater.log
+  exit 1
+fi
+
+gmcs -debug -out:VInt.exe ../VInt/Program.cs 2>&1 >>updater.log
+
+echo "READY"
+
+read -r steve
+
+mono VInt.exe &
